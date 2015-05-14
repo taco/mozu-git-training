d935a97446ee61c7b85608800bb2ff3a50a12d51
Taco <travis_johnson@volusion.com>
2015-05-14 15:31:59 -0500

    Changed router and added some simple rewrite rules

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 var app = require('koa')();
 var router = require('koa-router')();
 var serve = require('koa-static-folder');
+var rewrite = require('koa-rewrite');
 
 router
-	.get('/', function*(next) {
-		this.status = 307;
-		this.redirect('/app/index.html')
-	})
-	.get('/tasks', function*(next) {
+	// .get('/', function*(next) {
+	// 	this.status = 307;
+	// 	this.redirect('/app/index.html')
+	// })
+	.get('/api/tasks', function*(next) {
 		this.body = 'Hello Stuff!';
 	})
 	.post('/tasks', function*(next) {
@@ -21,9 +22,10 @@ router
 	});
 
 
-
 app
+	.use(rewrite('/*.(html|js|css)', '/app/$1.$2'))
 	.use(serve('./app'))
+
 	.use(router.routes())
 	.use(router.allowedMethods())
 	.listen(3000);
